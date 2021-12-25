70aa08cb0cae472a98f6630742927449db026a5c
Amir <amir1226@gmail.com>
2021-12-24 23:39:27 -0500

    Updating CRUD function to work with postgres DB and psycopg2

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
+import time
 
 app = FastAPI()
 
@@ -15,26 +16,29 @@ class Post(BaseModel):
     content : str
     published : bool = True
 
-try:
-    # Come from .env file
-    BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    load_dotenv(os.path.join(BASEDIR, '.env'))    
-    DB_USERNAME = os.getenv('DATABASE_USER') 
-    DB_PASSWORD = os.getenv('DATABASE_PSW')
+while True:
+    try:
+        # Come from .env file
+        BASEDIR = os.path.abspath(os.path.dirname(__file__))
+        load_dotenv(os.path.join(BASEDIR, '.env'))    
+        DB_USERNAME = os.getenv('DATABASE_USER') 
+        DB_PASSWORD = os.getenv('DATABASE_PSW')
 
-    conn = psycopg2.connect(host = 'localhost', dbname='fastapi', 
-                            user=DB_USERNAME, password=DB_PASSWORD, port=5435, cursor_factory= RealDictCursor)
-    
-    cursor=conn.cursor()
-    print('Database connection was succesfull!')
-    
-except Exception as error:
-    print('Connecciont to database failed\nError: ', error)
+        conn = psycopg2.connect(host = 'localhost', dbname='fastapi', 
+                                user=DB_USERNAME, password=DB_PASSWORD, port=5435, cursor_factory= RealDictCursor)
+        
+        cursor=conn.cursor()
+        print('Database connection was succesfull!')
+        break
+        
+    except Exception as error:
+        print('Connecciont to database failed\nError: ', error)
+        time.sleep(2)
     
     
 
-posts = [{"id":1, "title":"First post", "content":"Contet test"},
-                      {"id":2, "title":"Second post", "content":"Pizza is the best!"}]
+# posts = [{"id":1, "title":"First post", "content":"Contet test"},
+#                      {"id":2, "title":"Second post", "content":"Pizza is the best!"}]
 
 @app.get("/")
 async def hello():
@@ -42,18 +46,22 @@ async def hello():
 
 @app.get("/posts")
 async def get_posts():
+    cursor.execute("SELECT * FROM posts")
+    posts = cursor.fetchall()
     return {"posts": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_post(new_post: Post):
-    post_dict = new_post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute('''INSERT INTO posts (title, content, published) 
+                   VALUES (%s,%s,%s) RETURNING * ''', (new_post.title, new_post.content, new_post.published))
+    post = cursor.fetchone()
+    conn.commit()
+    return {"data": post}
 
 @app.get('/posts/{id}') 
 async def get_post(id: int): # response: Response
-    post = find_post(id, posts)
+    cursor.execute('''SELECT * FROM posts WHERE id = %s''', (str(id),))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id {id} was not found")
@@ -63,29 +71,35 @@ async def get_post(id: int): # response: Response
 
 
 @app.put("/posts/{id}")
-async def update_post(id:int, updated_post: Post):
-    post_index = find_post_index(id, posts)
-    if post_index is None:
+async def update_post(id:int, post: Post):
+    
+    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s
+                    RETURNING *""",
+                    (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    if updated_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id {id} was not found")
-    updated_post = updated_post.dict()
-    updated_post["id"] = id
-    posts[post_index] = updated_post
+    conn.commit()
     return{"data": updated_post}
 
 
 @app.delete("/posts/{id}")
 async def delete_post(id:int):
-    post_index = find_post_index(id, posts)
-    if post_index is None:
+    cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id {id} was not found")
-    posts.pop(post_index)
+    conn.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+# Used with in memory data
+'''
 def find_post(id : int, iterator:List):
     return next((x for x in iterator if x["id"] == id), None)
 
 def find_post_index(id : int, iterator:List):
     return next((i for i,x in enumerate(iterator) if x["id"] == id), None)
+'''
