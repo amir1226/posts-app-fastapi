9a7355e3f9642687eb9ea11d47d01e1f5981ec74
Amir <amir1226@gmail.com>
2022-01-01 22:00:57 -0500

    Adding CORS config

--- app/main.py
+++ app/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 from .database import engine
 from . import models
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # SQLAlchemy engine to create tables, replace by alembic migrations
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
