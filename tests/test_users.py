fd4c43a5229a21f060d43f66a8873310c3dd9ef4
Amir <amir1226@gmail.com>
2022-01-06 18:24:06 -0500

    Separating database config from test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,36 +1,5 @@
-from fastapi.testclient import TestClient
-from app.database import get_db
-import pytest
-from app.main import app
 import app.schemas as schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.database import Base
-
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_user}:{settings.database_psw}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
-app.dependency_overrides[get_db] = override_get_db
-        
-
-
-@pytest.fixture
-def client():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    return TestClient(app)
+from .database import client, session
 
 def test_root(client):
     response = client.get('/')
