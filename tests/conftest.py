fb7f83b7bb8533e781a0953a1381b9bf86bd9d6a
Amir <amir1226@gmail.com>
2022-01-07 16:11:00 -0500

    Adding posts functions tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,8 @@ from app.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.database import Base
-
+from app.oauth2 import create_access_token
+from app import models
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_user}:{settings.database_psw}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
@@ -32,7 +33,7 @@ def client(session):
         finally:
             session.close()       
     app.dependency_overrides[get_db] = override_get_db
-    return TestClient(app)
+    yield TestClient(app)
 
 @pytest.fixture
 def test_user(client):
@@ -45,3 +46,55 @@ def test_user(client):
     new_user['password'] = user_data['password']
     
     return new_user
+
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "maria@gmail.com",
+                 "password": "maria123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id" : test_user['id']})
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": f"Bearer {token}" }
+    return client
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user2['id']
+    }]
+
+    posts = list(map(lambda post: models.Post(**post), posts_data))
+    session.add_all(posts)
+    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
+    session.commit()
+
+    posts = session.query(models.Post).all()
+    return posts
