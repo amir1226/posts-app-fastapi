c2f7c682b9426af2306058e66a9355a3d35b6e26
Amir <amir1226@gmail.com>
2022-01-06 20:21:13 -0500

    Addin login test

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,26 @@
+import pytest
+from app.oauth2 import ALGORITHM, SECRET_KEY
 import app.schemas as schemas
 from .database import client, session
+from jose import jwt
 
-def test_root(client):
-    response = client.get('/')
-    assert response.json().get('message') == 'Hello fastapi learner!'
-    assert response.status_code == 200
+# def test_root(client):
+#     response = client.get('/')
+#     assert response.json().get('message') == 'Hello fastapi learner!'
+#     assert response.status_code == 200
 
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "pepe2@test-email.com",
+                 "password": "pepe123"}
+    response = client.post('/users/', json=user_data)
+    assert response.status_code == 201
+    
+    new_user = response.json()
+    new_user['password'] = user_data['password']
+    
+    return new_user
+    
 def test_create_user(client):
     email = 'pepe2@test-email.com'
     password = 'pepe123'
@@ -16,3 +31,12 @@ def test_create_user(client):
     assert new_user.email == email
     assert response.status_code == 201
 
+def test_login_user(client, test_user):
+    response = client.post('/login', data={'username': test_user['email'], 'password': test_user['password']})
+    login_response = schemas.Token(**response.json())
+    
+    payload = jwt.decode(login_response.access_token, SECRET_KEY, algorithms=[ALGORITHM])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_response.token_type == "bearer"
+    assert response.status_code == 200
